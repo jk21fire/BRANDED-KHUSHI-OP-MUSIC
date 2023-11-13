7c6dc106f8b9256347c5d7b27620fab8e98bd240
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-13 19:47:09 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/tc_lottery_hack_mod_pridction")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/earn_money_by_vip")
+    "SUPPORT_GROUP", "https://t.me/tc_lottery_hack_mod_pridction")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
